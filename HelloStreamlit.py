a98253776ea86c93fbdd17e697a97f4e28938f31
xhoanaBalla <xhoana.balla10@gmail.com>
2022-04-21 09:57:09 +0200

    Added Multiselect option for revenue based on countries

--- HelloStreamlit.py
+++ HelloStreamlit.py
@@ -1,9 +1,11 @@
+from turtle import color, width
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 import pyodbc
+import plotly.express as px
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Project3 Team1 Visualization")
@@ -72,3 +74,21 @@ df_selected.plot.bar(x='CampaignName', y = ["Revenue","RevenueTarget"])
 plt.legend(loc='upper right', prop={'size': 7})
 plt.show()
 st.pyplot()
+
+st.subheader("MultiSelect Option - Revenue based on Country" )
+country_options = df['Country'].unique().tolist() #get the unique values to appear in the dropdown
+the_country = st.multiselect('Which country you would like to see?', country_options, ['Germany'])
+# df = df[df['Country']=='Country'] #filter the data based on the campaing
+
+filtered_data =chunk_test[chunk_test['Country'].isin(the_country)]
+
+# filter = df[df['Country']=='Country']
+#create a scatter plot
+fig = px.bar(filtered_data, x="Country", y="Revenue", color="Country", range_y=[0, 2500000])
+
+fig.update_layout(width=800)
+st.write(fig)
+
+st.subheader("Animation test section" )
+
+
